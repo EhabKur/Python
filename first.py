fecc27cd94d69fad2cad0b3fdb0570714680e1be
ehabKur <ehabkur@gmail.com>
2024-08-29 13:38:40 +0200

    until the 6

---
+++ first.py
@@ -0,0 +1,13 @@
+print("Hello Python"); print("Hello 2 Python")
+if True:
+     print("hello python")
+#----------------------6-----------------------
+# تحديد نوع البيانات 
+x = 10 ; print(type(x))   # <class 'int'>
+y = "Hello"  ; print(type(y))  # <class 'str'>                             
+z = [1, 2, 3] ; print(type(z))  # <class 'list'>
+print(type(100.08))           # <class 'float'>
+print(type( (1,2,6,3,8,9)))    #<class 'tuple'>
+print( type(  { "one":1 , "two" :2 , "three" :3 })) # <class 'dict'>
+print(type(2==2))               #<class 'bool'>
+#--------------------7--------------------------------------
